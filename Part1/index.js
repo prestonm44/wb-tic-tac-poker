020b9a73d69890a6a83f8bd56afed83c9dda660c
prestonm44 <preston.mitchell4444@gmail.com>
2024-05-12 15:00:44 -0600

    finished part 1

--- Part1/index.js
+++ Part1/index.js
@@ -1,8 +1,64 @@
-function play() {
-    console.log("Square was clicked")
-    const squares = document.querySelectorAll("#square").value;
-    for (square of squares) {
-        square.addEventListener("click", play);
+let player = 'X'
+
+function play(evt) {
+    const targetSquare = evt.target;
+    targetSquare.innerText = player
+    
+    if (player === 'X') {
+        player = '0';
+    } else {
+        player = 'X'
     }
+    const playerSpan = document.querySelector('#current-player');
+    playerSpan.innerText = player;
+
+    const winner = calculateWinner();
+    if (winner) {
+        alert(`${winner} is the winner!`);
+    } else if (isBoardFull()) {
+        alert("Cat's game!");
+    }
+    }
+
+function isBoardFull() {
+    for (const square of squares) {
+        if (square.innerText === '') {
+            return false;
+        }
+    }
+    return true;
 }
 
+
+function calculateWinner() {
+    const lines = [
+        // Horizontal lines
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        // Vertical lines
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        // Diagonal lines
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (const line of lines) {
+        const [a, b, c] = line;
+        const squareAText = squares[a].innerText;
+        const squareBText = squares[b].innerText;
+        const squareCText = squares[c].innerText;
+        if (squareAText !== '' && squareAText === squareBText && squareAText === squareCText) {
+        return squareAText;
+        }
+    }
+return undefined;
+}
+
+const squares = document.querySelectorAll(".square");
+
+for (const square of squares) {
+    square.addEventListener("click", play)
+}
---
+++ Part2/index.js
@@ -0,0 +1,5 @@
+const idInput = document.querySelector("#cardId")
+const colorInput = document.querySelector("#setAttribute")
+
+console.log(idInput);
+console.log(colorInput);
