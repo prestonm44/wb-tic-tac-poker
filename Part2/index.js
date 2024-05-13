f348e4b9810e9c3c095e8a60a2502edf3ee10b2e
prestonm44 <preston.mitchell4444@gmail.com>
2024-05-12 22:39:38 -0600

    hw done

--- Part2/index.js
+++ Part2/index.js
@@ -1,5 +1,9 @@
-const idInput = document.querySelector("#cardId")
-const colorInput = document.querySelector("#setAttribute")
+const idInput = document.querySelector("#idInput")
+const colorInput = document.querySelector("#colorInput")
 
-console.log(idInput);
-console.log(colorInput);
+function setCard() {
+    const card = document.querySelector(`#${idInput.value}`);
+    card.style.color = colorInput.value;
+}
+
+document.querySelector("#editCardButton").addEventListener('click', setCard);
